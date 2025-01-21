dbd9f1acdb70ccab98007165d09c9a23853a0460
JinzhiHu <141042834+JinzhiHu@users.noreply.github.com>
2025-01-20 22:03:32 -0500

    Add files via upload

---
+++ Brownian_motion.R
@@ -0,0 +1,26 @@
+## Task 1.0: Calculate the mu and sigma of the real stock returns
+
+Data <- read.csv("Changyu.csv")
+
+## Logged-change on adjusted close
+mu <- mean(diff(log(na.omit(Data$Adj.Close))))
+sigma <- sd(diff(log(na.omit(Data$Adj.Close))))
+
+## We adapt with the in-practice estimation for p, u, and d
+n = 100
+dt = 1 / 12 ## Monthly data
+u = exp(sigma * sqrt(dt))
+d = 1 / u
+pu = 1 / 2 + 1 / 2 * mu / sigma * sqrt(dt)
+
+## We generate a Brownian motion (Using cumsum)
+set.seed(123)
+W <- cumsum(c(0, rnorm(n, 0, dt)))
+
+## Brownian motion with drift mu and volatility sigma
+Xt <- mu * seq(from = 0, to = n * dt, by = dt) + sigma * W
+
+## Geometric Brownian motion
+Y0 <- as.numeric(Data$Open[nrow(Data)]) ## We get the opening of the first 
+                                        ##  trading day
+Yt <- Y0 * exp(Xt)
